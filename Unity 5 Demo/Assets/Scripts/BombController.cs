59d7213546f4acd0afd79424cd251e48b8567a88
Will <grunt-139@hotmail.com>
2015-05-11 11:04:30 -0400

    Demo Code Commit
    Removed Unity_Chan
    
    Brought in Animations from Unity's Mecanim Demo, kept a handful from
    Unity_Chan to demonstrate how you can swap out models but still use the
    animations
    
    Added a 2D example scene
    Added a 3D movement scene demonstrating the animator
    Added a 3D movement scene that uses a State Machine Behaviour script to
    activate the particle system
    
    Added a lighting scene

---
+++ Unity 5 Demo/Assets/Scripts/BombController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombController : MonoBehaviour {
+
+    private Animator animController;
+
+	// Use this for initialization
+	void Start () {
+
+        //Get the animator from the gameobject this script is attached to
+        animController = gameObject.GetComponent<Animator>();
+	}
+	
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Simple little demo of how to talk to the animator
+        //When we collide with something, we check if the animator is null or not
+        //If not we set the 'isPrimed' trigger which kicks off the explosion loop
+        if (animController != null)
+            animController.SetTrigger("isPrimed");
+    }
+}
---
+++ Unity 5 Demo/Assets/Scripts/DudeController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DudeController : MonoBehaviour {
+
+    private Animator animController;
+
+    void Start()
+    {
+        //Get the animator from the gameobject this script is attached to
+        animController = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //We will use the horizontal input to indicate direction (left, right)
+        //Then the vertical will represent 'speed' ie going forward
+        float direction = Input.GetAxis("Horizontal");
+        float speed = Input.GetAxis("Vertical");
+
+        //If the animator is not null we send the information to it
+        if (animController != null)
+        {
+            //Set the direction and speed variables within the animator
+            animController.SetFloat("Speed", speed);
+            animController.SetFloat("Direction", direction);
+
+            //If the jump button has been pressed and then released, we set the trigger and we fire the jump animation
+            if (Input.GetButtonUp("Jump"))
+                animController.SetTrigger("Jump");
+        }
+    }
+}
